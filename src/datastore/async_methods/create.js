4fac3e3773f42928b894507147ab679cb1f0b653
Kent C. Dodds <kent+github@doddsfamily.us>
2014-12-05 08:34:10 -0700

    Even better. These changes make it work. ＼(＾O＾)／  Can't say as much for the tests though... (◞‸◟；)

--- src/datastore/async_methods/create.js
+++ src/datastore/async_methods/create.js
@@ -11,58 +11,58 @@ function create(resourceName, attrs, options) {
   var rejectionError;
   if (!definition) {
     rejectionError = new DSErrors.NER(resourceName);
-  } else if (!DSUtils.isObject(attrs)) {
-    rejectionError = new DSErrors.IA('"attrs" must be an object!');
-  } else {
-    options = DSUtils._(definition, options);
   }
-  if (rejectionError) {
-    return new DSUtils.Promise(function (resolve, reject) {
-      reject(rejectionError);
-    });
+  if (definition) {
+    options = DSUtils._(definition, options);
+    if (options.upsert && (DSUtils.isString(attrs) || DSUtils.isNumber(attrs))) {
+      return _this.update(resourceName, attrs, _this.get(resourceName, attrs), options);
+    }
   }
 
-  if (definition && options.upsert && attrs[definition.idAttribute]) {
-    return _this.update(resourceName, attrs[definition.idAttribute], attrs, options);
-  } else {
-    return new DSUtils.Promise(function(resolve, reject) {
-      resolve(attrs);
-    })
-      .then(function (attrs) {
-        return options.beforeValidate.call(attrs, options, attrs);
-      })
-      .then(function (attrs) {
-        return options.validate.call(attrs, options, attrs);
-      })
-      .then(function (attrs) {
-        return options.afterValidate.call(attrs, options, attrs);
-      })
-      .then(function (attrs) {
-        return options.beforeCreate.call(attrs, options, attrs);
-      })
-      .then(function (attrs) {
-        if (options.notify) {
-          _this.emit(options, 'beforeCreate', DSUtils.copy(attrs));
-        }
-        return _this.getAdapter(options).create(definition, attrs, options);
-      })
-      .then(function (attrs) {
-        return options.afterCreate.call(attrs, options, attrs);
-      })
-      .then(function (attrs) {
-        if (options.notify) {
-          _this.emit(options, 'afterCreate', DSUtils.copy(attrs));
-        }
-        if (options.cacheResponse) {
-          var created = _this.inject(definition.name, attrs, options);
-          var id = created[definition.idAttribute];
-          _this.store[resourceName].completedQueries[id] = new Date().getTime();
-          return created;
-        } else {
-          return _this.createInstance(resourceName, attrs, options);
-        }
-      });
+  if (!DSUtils.isObject(attrs)) {
+    rejectionError = new DSErrors.IA('"attrs" must be an object!');
   }
+  return new DSUtils.Promise(function (resolve, reject) {
+    if (rejectionError) {
+      reject(rejectionError);
+    } else {
+      resolve(attrs);
+    }
+  })
+    .then(function (attrs) {
+      return options.beforeValidate.call(attrs, options, attrs);
+    })
+    .then(function (attrs) {
+      return options.validate.call(attrs, options, attrs);
+    })
+    .then(function (attrs) {
+      return options.afterValidate.call(attrs, options, attrs);
+    })
+    .then(function (attrs) {
+      return options.beforeCreate.call(attrs, options, attrs);
+    })
+    .then(function (attrs) {
+      if (options.notify) {
+        _this.emit(options, 'beforeCreate', DSUtils.copy(attrs));
+      }
+      return _this.getAdapter(options).create(definition, attrs, options);
+    })
+    .then(function (attrs) {
+      return options.afterCreate.call(attrs, options, attrs);
+    })
+    .then(function (attrs) {
+      if (options.notify) {
+        _this.emit(options, 'afterCreate', DSUtils.copy(attrs));
+      }
+      if (options.cacheResponse) {
+        var created = _this.inject(definition.name, attrs, options);
+        var id = created[definition.idAttribute];
+        _this.store[resourceName].completedQueries[id] = new Date().getTime();
+        return created;
+      } else {
+        return _this.createInstance(resourceName, attrs, options);
+      }
+    });
 }
 
 module.exports = create;
