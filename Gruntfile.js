f54a342e0a84793b9841f278c7920b5ad11f7cca
Jason Dobry <jason.dobry@gmail.com>
2015-01-04 12:14:25 -0700

    Closes #29.

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,6 +137,33 @@ module.exports = function (grunt) {
     grunt.file.write('dist/js-data.js', file);
   });
 
+  grunt.registerTask('version', function (filePath) {
+    var file = grunt.file.read(filePath);
+
+    file = file.replace(/<%= pkg\.version %>/gi, pkg.version);
+
+    var parts = pkg.version.split('-');
+    var numbers = parts[0].split('.');
+
+    file = file.replace(/<%= major %>/gi, numbers[0]);
+    file = file.replace(/<%= minor %>/gi, numbers[1]);
+    file = file.replace(/<%= patch %>/gi, numbers[2]);
+
+    if (pkg.version.indexOf('alpha') !== -1) {
+      file = file.replace(/<%= alpha %>/gi, parts[1].replace('alpha.', '') + (parts.length > 2 ? '-' + parts[2] : ''));
+    } else {
+      file = file.replace(/<%= alpha %>/gi, false);
+    }
+
+    if (pkg.version.indexOf('beta') !== -1) {
+      file = file.replace(/<%= beta %>/gi, parts[1].replace('beta.', '') + (parts.length > 2 ? '-' + parts[2] : ''));
+    } else {
+      file = file.replace(/<%= beta %>/gi, false);
+    }
+
+    grunt.file.write(filePath, file);
+  });
+
   grunt.registerTask('n', ['mochaTest']);
   grunt.registerTask('b', ['karma:ci', 'karma:min']);
   grunt.registerTask('w', ['n', 'watch:n']);
@@ -146,6 +173,7 @@ module.exports = function (grunt) {
     'clean',
     'jshint',
     'browserify',
+    'version:dist/js-data.js',
     'banner',
     'uglify:main'
   ]);
--- src/index.js
+++ src/index.js
@@ -6,5 +6,13 @@ module.exports = {
     return new DS(options);
   },
   DSUtils: require('./utils'),
-  DSErrors: require('./errors')
+  DSErrors: require('./errors'),
+  version: {
+    full: '<%= pkg.version %>',
+    major: parseInt('<%= major %>', 10),
+    minor: parseInt('<%= minor %>', 10),
+    patch: parseInt('<%= patch %>', 10),
+    alpha: '<%= alpha %>' !== 'false' ? '<%= alpha %>' : false,
+    beta: '<%= beta %>' !== 'false' ? '<%= beta %>' : false
+  }
 };
