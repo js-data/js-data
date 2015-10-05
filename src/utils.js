c932de5d711a1d195e574f9fb327892a549b4bf5
Warren Taylor <warren@warrentaylor.ca>
2015-10-05 10:05:08 -0700

    finishing merge

--- src/utils.js
+++ src/utils.js
@@ -655,7 +655,7 @@ export default {
             var _this2 = this
 
             if (children && children.length) {
-              let id = get(this, definition.idAttribute)
+              let id = get(_this2, definition.idAttribute)
               if (foreignKey) {
                 forEach(children, function (child) {
                   set(child, foreignKey, id)
