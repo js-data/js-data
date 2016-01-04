e36d0242a7b03d6cbb2c522f290d404f22032b0a
Robert P <rob@Roberts-MacBook-Pro.local>
2016-01-04 12:01:14 +1030

    Update to the typings

--- js-data.d.ts
+++ js-data.d.ts
@@ -11,7 +11,7 @@ declare module JSData {
     /**
      * Create a new secondary index on the contents of the collection.
      */
-    createIndex(name: string, keyList: string[]): this;
+    createIndex(name: string, keyList: string[]): Collection<R>;
 
     /**
      * Create a new query to be executed against the contents of the collection.
@@ -53,7 +53,7 @@ declare module JSData {
     /**
      * Iterate over all entities.
      */
-    forEach(fn: Function, thisArg);
+    forEach(fn: Function, thisArg?);
 
     /**
      * Reduce the data in the collection to a single value and return the result.
@@ -63,7 +63,7 @@ declare module JSData {
     /**
      * Apply a mapping function to all entities.
      */
-    map<T>(fn: Function, thisArg): T[];
+    map<T>(fn: (each: R) => T, thisArg?): T[];
 
     /**
      * Instead a record into this collection, updating all indexes with the new
@@ -105,43 +105,43 @@ declare module JSData {
     /**
      * Find all entities between two boundaries.
      */
-    between(leftKeys: string[], rightKeys: string[], options: QueryOptions): this;
+    between(leftKeys: string[], rightKeys: string[], options: QueryOptions): Query<R>;
 
     /**
      * Find the entity or entities that match the provided key.
      */
-    get(keyList: string[], options: QueryOptionsBase): this;
+    get(keyList: string[], options: QueryOptionsBase): Query<R>;
 
     /**
      * Find the entity or entities that match the provided keyLists.
      */
-    getAll(keyList: string[], options: QueryOptionsBase): this;
+    getAll(keyList: string[], options: QueryOptionsBase): Query<R>;
 
     /**
      * Find the entity or entities that match the provided query or pass the
      * provided filter function.
      */
-    filter(query: Object | Function, thisArg): this;
+    filter(query: Object | Function, thisArg?): Query<R>;
 
     /**
      * Skip a number of results.
      */
-    skip(number: Number): this;
+    skip(number: Number): Query<R>;
 
     /**
      * Limit the result.
      */
-    limit(number: Number): this;
+    limit(number: Number): Query<R>;
 
     /**
      * Iterate over all entities.
      */
-    forEach(fn: Function, thisArg): this;
+    forEach(fn: Function, thisArg?): Query<R>;
 
     /**
      * Apply a mapping function to the result data.
      */
-    map(fn: Function, thisArg): this;
+    map(fn: Function, thisArg?): Query<R>;
 
     /**
      * Complete the execution of the query and return the resulting data.
@@ -217,181 +217,12 @@ declare module JSData {
    * js-data's Model class.
    */
   class Model<R> {
-    new(properties: R, options: ModelOptions): this;
 
     /**
      * Create a new secondary index in the Collection instance of this Model.
      */
     static createIndex(name: string, keyList: string[]);
 
-    /**
-     * Create a new instance of this Model from the provided properties.
-     */
-    static createInstance<R>(props: Object): Model<R>;
-
-    /**
-     * Check whether "instance" is actually an instance of this Model.
-     */
-    static is(instance): boolean;
-
-    /**
-     * Insert the provided item or items into the Collection instance of this
-     * Model.
-     *
-     * If an item is already in the collection then the provided item will either
-     * merge with or replace the existing item based on the value of the
-     * "onConflict" option.
-     *
-     * The collection's secondary indexes will be updated as each item is visited.
-     */
-    static inject<R>(items: Model<R> | Object, options?: Object): Model<R>;
-
-    /**
-     * Insert the provided item or items into the Collection instance of this
-     * Model.
-     *
-     * If an item is already in the collection then the provided item will either
-     * merge with or replace the existing item based on the value of the
-     * "onConflict" option.
-     *
-     * The collection's secondary indexes will be updated as each item is visited.
-     */
-    static inject<R>(items: Model<R>[] | Object[], options?: Object): Model<R>[];
-
-    /**
-     * Remove the instance with the given primary key from the Collection instance
-     * of this Model.
-     */
-    static eject<R>(id: ID): Model<R> | void;
-
-    /**
-     * Remove the instances selected by "query" from the Collection instance of
-     * this Model.
-     */
-    static ejectAll<R>(query: Object): Model<R>[];
-
-    /**
-     * Return the instance in the Collection instance of this Model that has
-     * the given primary key, if such an instance can be found.
-     */
-    static get<R>(id: ID): Model<R> | void;
-
-    /**
-     * Proxy for Collection.between()
-     */
-    static getAll<R>(keyList: string[], options: QueryOptionsBase): R[];
-
-    /**
-     * Proxy for Collection.filter()
-     */
-    static filter<R>(query: Object | Function): R[];
-
-    /**
-     * Proxy for `Collection.query()`.
-     */
-    static query<R>(): Query<R>;
-
-    /**
-     * Return the registered adapter with the given name or the default adapter if
-     * no name is provided.
-     */
-    static getAdapter(name?: string): Adapter;
-
-    /**
-     * Return the name of a registered adapter based on the given name or options,
-     * or the name of the default adapter if no name provided
-     */
-    static getAdapterName(options: Object): string;
-
-    /**
-     * Lifecycle hook. Called by `Model.create` after `Model.create` checks
-     * whether it can do an upsert and before `Model.create` calls the `create`
-     * method of an adapter.
-     *
-     * `Model.beforeCreate` will receive the same arguments that are passed to
-     * `Model.create`. If `Model.beforeCreate` returns a promise, `Model.create`
-     * will wait for the promise to resolve before continuing. If the promise
-     * rejects, then the promise returned by `Model.create` will reject. If
-     * `Model.beforeCreate` does not return a promise, `Model.create` will resume
-     * execution immediately.
-     */
-    static beforeCreate(props?: Object, opts?: Object): Promise<void> | void;
-
-    /**
-     * The "C" in "CRUD", `Model.create` creates a single entity using the
-     * `create` method of an adapter. If the `props` passed to `Model.create`
-     * contain a primary key as configured by `Model.idAttribute` and
-     * `opts.upsert` is `true` of `Model.upsert` is `true` and `opts.upsert` is
-     * not `false`, then `Model.update` will be called instead.
-     *
-     * 1. `Model.beforeCreate` is called and passed the same arguments passed to
-     * `Model.create`.
-     * 1. `props` and `opts` are passed to the `create` method of the adapter
-     * specified by `opts.adapter` or `Model.defaultAdapter`.
-     * 1. `Model.afterCreate` is called with the `data` argument returned by the
-     * adapter's `create` method and the `opts` argument passed to `Model.create`.
-     * 1. If `opts.raw` is `true` or `Model.raw` is `true` and `opts.raw` is not
-     * `false`, then a result object is returned that contained the created entity
-     * and some metadata about the operation and its result. Otherwise, the
-     * promise returned by `Model.create` resolves with the created entity.
-     */
-    static create<R>(props?: Object, opts?: Object): Promise<R>;
-
-    /**
-     * Lifecycle hook. Called by `Model.create` after `Model.create` call the
-     * `create` method of an adapter.
-     *
-     * `Model.afterCreate` will receive the `data` argument returned by the
-     * adapter's `create` method and the `opts` argument passed to `Model.create`.
-     * If `Model.afterCreate` returns a promise, `Model.create` will wait for the
-     * promise to resolve before continuing. If the promise rejects, then the
-     * promise returned by `Model.create` will reject. If `Model.afterCreate` does
-     * not return a promise, `Model.create` will resume execution immediately.
-     */
-    static afterCreate(data?: Object, opts?: Object): Promise<void> | void;
-
-    static beforeCreateMany(items?: Object[], opts?: Object): Promise<void> | void;
-    static createMany<R>(items?: Object[], opts?: Object): Promise<R[]>;
-    static afterCreateMany(data?: Object[], opts?: Object): Promise<void> | void;
-    static beforeFind(id?: ID, opts?: Object): Promise<void> | void;
-    static find<R>(id: ID, opts?: Object): Promise<R>;
-    static afterFind(data?: Object, opts?: Object): Promise<void> | void;
-    static beforeFindAll(query?: Object, opts?: Object): Promise<void> | void;
-    static findAll<R>(query?: Object, opts?: Object): Promise<R[]>;
-    static afterFindAll(query?: Object, opts?: Object): Promise<void> | void;
-    static beforeUpdate(id?: ID, props?: Object, opts?: Object): Promise<void> | void;
-    static update<R>(id: ID, props?: Object, opts?: Object): Promise<R>;
-    static afterUpdate(id?: ID, props?: Object, opts?: Object): Promise<void> | void;
-    static beforeUpdateMany(items?: Object[], opts?: Object): Promise<void> | void;
-    static updateMany<R>(items?: Object[], opts?: Object): Promise<R[]>;
-    static afterUpdateMany(data?: Object[], opts?: Object): Promise<void> | void;
-    static beforeUpdateAll(query?: Object, props?: Object, opts?: Object): Promise<void> | void;
-    static updateAll<R>(query?: Object, props?: Object, opts?: Object): Promise<R[]>;
-    static afterUpdateAll(query?: Object, data?: Object[], opts?: Object): Promise<void> | void;
-    static beforeDestroy(id?: ID, opts?: Object): Promise<void> | void;
-    static destroy<R>(id: ID, opts?: Object): Promise<R>;
-    static afterDestroy(data?: Object, opts?: Object): Promise<void> | void;
-    static beforeDestroyAll(query?: Object, opts?: Object): Promise<void> | void;
-    static destroyAll<R>(query?: Object, opts?: Object): Promise<R[]>;
-    static afterDestroyAll(query?: Object, opts?: Object): Promise<void> | void;
-    static belongsTo(model: Model<any>, opts?: RelationOptions): any;
-    static hasMany(model: Model<any>, opts?: RelationOptions): any;
-    static hasOne(model: Model<any>, opts?: RelationOptions): any;
-
-    /**
-     * Invoke the `setSchema` decorator on this Model.
-     */
-    static setSchema(opts?: Object): any;
-
-    /**
-     * Invoke the `configure` decorator on this Model.
-     */
-    static configure(opts?: Object): any;
-
-    /**
-     * Invoke the `registerAdapter` decorator on this Model.
-     */
-    static registerAdapter(name: string, adapter: Adapter, opts?: Object): Model<any>;
 
     schema(key?: string): Object;
     validate(obj?: Object): string[] | void;
@@ -414,6 +245,184 @@ declare module JSData {
      * Return a plain object representation of this instance.
      */
     toJSON(opts?: Object): string;
+  }
+
+  interface ModelStatic<R> {
+    new (properties: R, options: ModelOptions): Model<R>;
+
+    /**
+     * Create a new secondary index in the Collection instance of this Model.
+     */
+    createIndex(name: string, keyList: string[]);
+
+    /**
+     * Create a new instance of this Model from the provided properties.
+     */
+    createInstance(props: Object): Model<R>;
+
+    /**
+     * Check whether "instance" is actually an instance of this Model.
+     */
+    is(instance): boolean;
+
+    /**
+     * Insert the provided item or items into the Collection instance of this
+     * Model.
+     *
+     * If an item is already in the collection then the provided item will either
+     * merge with or replace the existing item based on the value of the
+     * "onConflict" option.
+     *
+     * The collection's secondary indexes will be updated as each item is visited.
+     */
+    inject(items: Model<R> | Object, options?: Object): Model<R>;
+
+    /**
+     * Insert the provided item or items into the Collection instance of this
+     * Model.
+     *
+     * If an item is already in the collection then the provided item will either
+     * merge with or replace the existing item based on the value of the
+     * "onConflict" option.
+     *
+     * The collection's secondary indexes will be updated as each item is visited.
+     */
+    inject(items: Model<R>[] | Object[], options?: Object): Model<R>[];
+
+    /**
+     * Remove the instance with the given primary key from the Collection instance
+     * of this Model.
+     */
+    eject(id: ID): Model<R> | void;
+
+    /**
+     * Remove the instances selected by "query" from the Collection instance of
+     * this Model.
+     */
+    ejectAll(query: Object): Model<R>[];
+
+    /**
+     * Return the instance in the Collection instance of this Model that has
+     * the given primary key, if such an instance can be found.
+     */
+    get(id: ID): Model<R> | void;
+
+    /**
+     * Proxy for Collection.between()
+     */
+    getAll(keyList: string[], options: QueryOptionsBase): R[];
+
+    /**
+     * Proxy for Collection.filter()
+     */
+    filter(query: Object | Function): R[];
+
+    /**
+     * Proxy for `Collection.query()`.
+     */
+    query(): Query<R>;
+
+    /**
+     * Return the registered adapter with the given name or the default adapter if
+     * no name is provided.
+     */
+    getAdapter(name?: string): Adapter;
+
+    /**
+     * Return the name of a registered adapter based on the given name or options,
+     * or the name of the default adapter if no name provided
+     */
+    getAdapterName(options: Object): string;
+
+    /**
+     * Lifecycle hook. Called by `Model.create` after `Model.create` checks
+     * whether it can do an upsert and before `Model.create` calls the `create`
+     * method of an adapter.
+     *
+     * `Model.beforeCreate` will receive the same arguments that are passed to
+     * `Model.create`. If `Model.beforeCreate` returns a promise, `Model.create`
+     * will wait for the promise to resolve before continuing. If the promise
+     * rejects, then the promise returned by `Model.create` will reject. If
+     * `Model.beforeCreate` does not return a promise, `Model.create` will resume
+     * execution immediately.
+     */
+    beforeCreate(props?: Object, opts?: Object): Promise<void> | void;
+
+    /**
+     * The "C" in "CRUD", `Model.create` creates a single entity using the
+     * `create` method of an adapter. If the `props` passed to `Model.create`
+     * contain a primary key as configured by `Model.idAttribute` and
+     * `opts.upsert` is `true` of `Model.upsert` is `true` and `opts.upsert` is
+     * not `false`, then `Model.update` will be called instead.
+     *
+     * 1. `Model.beforeCreate` is called and passed the same arguments passed to
+     * `Model.create`.
+     * 1. `props` and `opts` are passed to the `create` method of the adapter
+     * specified by `opts.adapter` or `Model.defaultAdapter`.
+     * 1. `Model.afterCreate` is called with the `data` argument returned by the
+     * adapter's `create` method and the `opts` argument passed to `Model.create`.
+     * 1. If `opts.raw` is `true` or `Model.raw` is `true` and `opts.raw` is not
+     * `false`, then a result object is returned that contained the created entity
+     * and some metadata about the operation and its result. Otherwise, the
+     * promise returned by `Model.create` resolves with the created entity.
+     */
+    create(props?: Object, opts?: Object): Promise<R>;
+
+    /**
+     * Lifecycle hook. Called by `Model.create` after `Model.create` call the
+     * `create` method of an adapter.
+     *
+     * `Model.afterCreate` will receive the `data` argument returned by the
+     * adapter's `create` method and the `opts` argument passed to `Model.create`.
+     * If `Model.afterCreate` returns a promise, `Model.create` will wait for the
+     * promise to resolve before continuing. If the promise rejects, then the
+     * promise returned by `Model.create` will reject. If `Model.afterCreate` does
+     * not return a promise, `Model.create` will resume execution immediately.
+     */
+    afterCreate(data?: Object, opts?: Object): Promise<void> | void;
+
+    beforeCreateMany(items?: Object[], opts?: Object): Promise<void> | void;
+    createMany(items?: Object[], opts?: Object): Promise<R[]>;
+    afterCreateMany(data?: Object[], opts?: Object): Promise<void> | void;
+    beforeFind(id?: ID, opts?: Object): Promise<void> | void;
+    find(id: ID, opts?: Object): Promise<R>;
+    afterFind(data?: Object, opts?: Object): Promise<void> | void;
+    beforeFindAll(query?: Object, opts?: Object): Promise<void> | void;
+    findAll(query?: Object, opts?: Object): Promise<R[]>;
+    afterFindAll(query?: Object, opts?: Object): Promise<void> | void;
+    beforeUpdate(id?: ID, props?: Object, opts?: Object): Promise<void> | void;
+    update(id: ID, props?: Object, opts?: Object): Promise<R>;
+    afterUpdate(id?: ID, props?: Object, opts?: Object): Promise<void> | void;
+    beforeUpdateMany(items?: Object[], opts?: Object): Promise<void> | void;
+    updateMany(items?: Object[], opts?: Object): Promise<R[]>;
+    afterUpdateMany(data?: Object[], opts?: Object): Promise<void> | void;
+    beforeUpdateAll(query?: Object, props?: Object, opts?: Object): Promise<void> | void;
+    updateAll(query?: Object, props?: Object, opts?: Object): Promise<R[]>;
+    afterUpdateAll(query?: Object, data?: Object[], opts?: Object): Promise<void> | void;
+    beforeDestroy(id?: ID, opts?: Object): Promise<void> | void;
+    destroy(id: ID, opts?: Object): Promise<R>;
+    afterDestroy(data?: Object, opts?: Object): Promise<void> | void;
+    beforeDestroyAll(query?: Object, opts?: Object): Promise<void> | void;
+    destroyAll(query?: Object, opts?: Object): Promise<R[]>;
+    afterDestroyAll(query?: Object, opts?: Object): Promise<void> | void;
+    belongsTo(model: Model<any>, opts?: RelationOptions): any;
+    hasMany(model: Model<any>, opts?: RelationOptions): any;
+    hasOne(model: Model<any>, opts?: RelationOptions): any;
+
+    /**
+     * Invoke the `setSchema` decorator on this Model.
+     */
+    setSchema(opts?: Object): any;
+
+    /**
+     * Invoke the `configure` decorator on this Model.
+     */
+    configure(opts?: Object): any;
+
+    /**
+     * Invoke the `registerAdapter` decorator on this Model.
+     */
+    registerAdapter(name: string, adapter: Adapter, opts?: Object): ModelStatic<R>;
   }
 
   interface Adapter { }
@@ -490,7 +499,7 @@ declare module JSData {
     /**
      * Iterate over an object's own enumerable properties.
      */
-    function forOwn(object: Object, fn: Function, thisArg);
+    function forOwn(object: Object, fn: Function, thisArg?);
 
     /**
      * Recursively shallow copy own enumberable properties from `src` to `dest`.
@@ -508,9 +517,9 @@ declare module JSData {
     function reject(value): Promise<any>;
 
     /**
-     * Shallow copy own enumerable non-function properties from `model` to `options`.
+     * Shallow copy own enumerable non-function properties from `src` to `dest`.
      */
-    function _<R>(model: Model<R>, options: Object);
+    function _(src: Object, dest: Object);
 
     /**
      * Return the intersection of two arrays.
