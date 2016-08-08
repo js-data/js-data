64c4aee4eec6217d19ef89d69b6549073d4c530c
pik <alexander.maznev@gmail.com>
2016-08-08 01:55:45 -0300

    Add additional test in hasChanges for event timing

--- test/unit/record/hasChanges.test.js
+++ test/unit/record/hasChanges.test.js
@@ -1,4 +1,4 @@
-import { assert, JSData } from '../../_setup'
+import { assert, JSData, sinon } from '../../_setup'
 
 describe('Record#hasChanges', function () {
   it('should be an instance method', function () {
@@ -13,7 +13,7 @@ describe('Record#hasChanges', function () {
     post.author = 'Jake'
     assert(post.hasChanges())
   })
-  it('should return true if a tracked field is changed', function () {
+  it('should return true if a tracked field is changed', function (done) {
     const PostMapper = new JSData.Mapper({
       name: 'post',
       schema: {
@@ -26,10 +26,51 @@ describe('Record#hasChanges', function () {
       }
     })
     const post = PostMapper.createRecord(this.data.p1)
+    const listener = sinon.stub()
+    post.on('change', listener)
     assert(!post.hasChanges())
     post.author = 'Jake'
     assert(post.hasChanges())
     post.author = 'John'
     assert(!post.hasChanges())
+    setTimeout(function() {
+      assert.equal(listener.callCount, 0)
+      done()
+    }, 5)
+  }),
+
+  /* The previous test has a property set and changed back within a single event loop
+  * So no listener is ever called.
+  * This test checks that hasChanges() is still false (if the state is set back to the previous)
+  * even if both changes were registered and a listener was called on each change (twice in total).
+  */
+
+  it('is not affected by timing', function(done) {
+    const PostMapper = new JSData.Mapper({
+      name: 'post',
+      schema: {
+        properties: {
+          author: {
+            type: 'string',
+            track: true
+          }
+        }
+      }
+    })
+    const post = PostMapper.createRecord(this.data.p1)
+    const listener = sinon.stub()
+    post.on('change', listener)
+    post.author = 'Jake'
+    assert(post.hasChanges())
+    const secondSpec = function() {
+      assert.equal(listener.callCount, 2)
+      assert(!post.hasChanges())
+      done()
+    }
+    setTimeout(function () {
+      assert.equal(listener.callCount, 1)
+      post.author = 'John'
+      setTimeout(secondSpec, 5)
+    }, 5)
   })
 })
