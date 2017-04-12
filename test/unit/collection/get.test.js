52ad2307b9c55fd1a80dba8487d88e202f28f361
Ivan Voznyakovsky <ivan.voznyakovsky@gmail.com>
2017-04-12 14:44:15 +0300

    fix(Collection): 'get' to return undefined when item id is not provided

--- test/unit/collection/get.test.js
+++ test/unit/collection/get.test.js
@@ -5,5 +5,6 @@ describe('Collection#get', function () {
     const user = this.UserCollection.add({ id: 1 })
     assert(this.UserCollection.get(1) === user, 'should get the user from the collection')
     assert(!this.UserCollection.get(2), 'should return undefined if the item is not in the collection')
+    assert(!this.UserCollection.get(), 'should return undefined if item id is not provided')
   })
 })
