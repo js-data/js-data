d240e345148b0a419cf7fb40afd2cd51295292b3
Jeremy Trufier <jeremy@trufier.com>
2017-06-06 10:11:52 +0200

    Fixes #471

--- test/unit/utils/deepEqual.test.js
+++ test/unit/utils/deepEqual.test.js
@@ -8,13 +8,19 @@ describe('utils.deepEqual', function () {
 
   it('does deep equal comparison', function () {
     const objA = { name: 'John', age: 90 }
+    const arrA = ['a', 'b', 'c']
+    const arrB = ['a', 'b', 'c', 'd', 'e']
+
     assert.isTrue(utils.deepEqual(2, 2), '2 deep equals 2')
     assert.isTrue(utils.deepEqual('test', 'test'), '"test" deep equals "test"')
     assert.isTrue(utils.deepEqual({}, {}), '{} deep equals {}')
     assert.isTrue(utils.deepEqual(objA, objA), objA + ' deep equals ' + objA)
+    assert.isTrue(utils.deepEqual(arrA, arrA), 'arrA deep equals arrA')
+
     assert.isFalse(utils.deepEqual(1, 2), '1 does not strict equal 2')
     assert.isFalse(utils.deepEqual(1, '1'), '1 does not strict equal "1"')
     assert.isFalse(utils.deepEqual('foo', 'bar'), '"foo" does not equal "bar')
+    assert.isFalse(utils.deepEqual(arrA, arrB), 'arrA does not deep equal arrB')
   })
 
   it('compares identical objects', function () {
